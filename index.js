1325db2455f8333ee69bfc540558e060eecee21d
Omogod <godstimeomoregie0@gmail.com>
2022-09-12 22:13:09 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-function listSorting(needle, haystack) {
-    
-    //check if haystack is an array of arrays
-    if(typeof(haystack[0]) == "object") {
-        let column;
-        let row;
+/**
+ * Implement the solution in this function
+ *
+ *  * @param {string} value
+ */
 
+    function binaryReversal(value) {
+        // convert value to binary
+        let binary = Number(value).toString(2);
+
+        // pad the binary with 0s to make it 8 digits
+        let paddedBinary = binary.padStart(8, '0');
+
+        // reverse the padded binary
+        let reversedBinary = paddedBinary.split('').reverse().join('');
+
+        // convert the reversed binary to decimal (base 10)
+        let decimalB = parseInt(reversedBinary, 2)
+
+        return decimalB.toString();
         
-        //loop through haystack to find the row that contains the needle
-    for (let i = 0; i < haystack.length; i++) {
-        
-        //loop through each row to find the column that contains the needle
-        for (let j = 0; j < haystack[i].length; j++) {
-            
-            //if the needle is found, store the column and row
-            if (haystack[i][j] === needle) {
-                column = haystack[i].lastIndexOf(needle);
-                row = i;
-                
-            }
-        }
     }
-    //check if needle is in any row and return the column and row
-    if (row == undefined) return -1;
-    return [row, column];   
-    }
+console.log(binaryReversal('47'));
 
-    //return column or needle index if haystack is not an array of arrays
-    else {
-        return haystack.lastIndexOf(needle);
-    }
-}
-
-
-module.exports = listSorting;
-
+module.exports = binaryReversal;
